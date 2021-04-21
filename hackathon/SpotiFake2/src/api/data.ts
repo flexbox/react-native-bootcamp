044129430d7db54455e81602f5c97889f86cd51a
David Leuliette <dleuliette@gmail.com>
2021-04-21 09:41:44 +0200

    Add mp3

---
+++ hackathon/SpotiFake2/src/api/data.ts
@@ -0,0 +1,21 @@
+
+export const allSounds = [
+  {
+    artist: "Harry Gregson Williams",
+    category: "OST",
+    music: {
+      title: "Solid Main Theme",
+      url: "Harry-Gregson-Williams-Metal-Gear-Solid-Main-Theme.mp3",
+      duration: "22sec"
+    }
+  },
+  {
+    artist: "Harry Gregson Williams",
+    category: "OST",
+    music: {
+      title: "Metal Gear Solid 3: Snake Eater",
+      url: "Harry-Gregson-Williams-Metal-Gear-Solid-Main-Theme.mp3",
+      duration: "22sec"
+    }
+  }
+]
