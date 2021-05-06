cf28f30c49cf3f36a8d303f7a8e2c21099cf0287
David Leuliette <dleuliette@gmail.com>
2021-05-06 13:39:35 +0200

    update routes for Navigator

--- hackathon/nerd-fm/App.tsx
+++ hackathon/nerd-fm/App.tsx
@@ -1,15 +1,13 @@
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { RootStack } from './src/navigation/Navigator';
+
+import Navigator from './src/navigation/Navigator';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <PaperProvider>
-        <RootStack />
-      </PaperProvider>
-    </NavigationContainer>
+    <PaperProvider>
+      <Navigator />
+    </PaperProvider>
   );
 }
--- hackathon/nerd-fm/src/navigation/Navigator.tsx
+++ hackathon/nerd-fm/src/navigation/Navigator.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import MusicDetailsScreen from '../screens/MusicDetailsScreen';
 import MusicFeedScreen from '../screens/MusicFeedScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
-export function RootStack() {
+function RootStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Music Feed" component={MusicFeedScreen} />
@@ -13,3 +14,13 @@ export function RootStack() {
     </Stack.Navigator>
   );
 }
+
+function Navigator() {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
+}
+
+export default Navigator;
