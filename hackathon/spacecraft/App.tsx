a015721ec5b5ac3b6f9609ffd30bb36c509dfa6a
David Leuliette <dleuliette@gmail.com>
2021-05-18 12:09:53 +0200

    update content closes #10

--- hackathon/spacecraft/App.tsx
+++ hackathon/spacecraft/App.tsx
@@ -3,14 +3,12 @@ import { StarshipScreen } from './src/screen';
 import { PilotScreen } from './src/screen';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-// Data Management: 🤸‍♀️ Exercise 2 `QueryClientProvider` should be here
-
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <PilotScreen />
+      <StarshipScreen />
     </QueryClientProvider>
   );
 };
--- hackathon/spacecraft/src/screen/StarshipScreen.tsx
+++ hackathon/spacecraft/src/screen/StarshipScreen.tsx
@@ -3,6 +3,7 @@ import { useQuery } from 'react-query';
 import fetchAsync from '../lib/fetchAsync';
 import AppLayout from '../components/AppLayout';
 import StarshipCard from '../components/StarshipCard';
+import { FlatList } from 'react-native';
 
 interface shipProps {
   name: string;
@@ -11,17 +12,31 @@ interface shipProps {
   cost_in_credits: string;
 }
 
+// SOLUTION 2: with a FlatList - more performant
+// const renderItem = ({ item }: shipProps) => {
+//   return (
+//     <StarshipCard
+//       key={item.name}
+//       title={item.name}
+//       subtitle={item.model}
+//       manufacturer={item.manufacturer}
+//       price={item.cost_in_credits}
+//     />
+//   );
+// };
+
 const StarshipScreen = () => {
   const { isLoading, isError, data } = useQuery('starships', () =>
     fetchAsync(`https://swapi.dev/api/starships/`)
   );
 
-  if (isLoading) return <AppLayout title="Loading..." />;
+  if (isLoading) return <AppLayout title="Loading…" />;
   if (isError) return <AppLayout title="Error 😕" />;
   if (data.results === undefined) return <AppLayout title="Not Found" />;
 
   return (
     <AppLayout title="Starships">
+      {/* SOLUTION 1: with a map */}
       {data.results.map((ship: shipProps) => {
         return (
           <StarshipCard
@@ -33,6 +48,13 @@ const StarshipScreen = () => {
           />
         );
       })}
+
+      {/* SOLUTION 2: with a FlatList - more performant */}
+      {/* <FlatList
+        data={data.results}
+        renderItem={renderItem}
+        keyExtractor={(ship) => ship.model}
+      /> */}
     </AppLayout>
   );
 };
