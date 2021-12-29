617a91230b4008222f6c58c7c7bdf045c837bfc7
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-12-29 11:38:29 +0100

    add all keyboard type

--- hackathon/allKeyboard/App.js
+++ hackathon/allKeyboard/App.js
@@ -1,20 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, ScrollView, StyleSheet, TextInput } from "react-native";
 
-export default function App() {
+const UselessTextInput = () => {
+  const [data, onChange] = React.useState(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.global}>
+      <ScrollView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="Input"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="numeric"
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="email-address"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="phone-pad"
+          keyboardType="phone-pad"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="ascii-capable"
+          keyboardType="ascii-capable"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="numbers-and-punctuation"
+          keyboardType="numbers-and-punctuation"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="url"
+          keyboardType="url"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="number-pad"
+          keyboardType="number-pad"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="name-phone-pad"
+          keyboardType="name-phone-pad"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="decimal-pad"
+          keyboardType="decimal-pad"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="twitter"
+          keyboardType="twitter"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="web-search"
+          keyboardType="web-search"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChange}
+          value={data}
+          placeholder="visible-password"
+          keyboardType="visible-password"
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  global: {
+    paddingTop: 50,
   },
 });
+
+export default UselessTextInput;
