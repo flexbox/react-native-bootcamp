bd4d6c642329bab0b8909f91c706baffbd495e20
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-12-31 14:14:16 +0100

    change var

--- hackathon/allKeyboard/App.tsx
+++ hackathon/allKeyboard/App.tsx
@@ -3,7 +3,7 @@ import { TextInput } from "react-native-paper";
 import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
 
 const UselessTextInput = () => {
-  const [data, onChange] = React.useState(null);
+  const [inputValue, setInputValue] = React.useState(null);
   const [password, setPassword] = React.useState("");
   const [isVisble, setIsVisible] = React.useState(true);
 
@@ -16,14 +16,14 @@ const UselessTextInput = () => {
       <ScrollView>
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="Input"
         />
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="numeric"
           keyboardType="numeric"
         />
@@ -43,78 +43,78 @@ const UselessTextInput = () => {
         />
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="email-address"
           keyboardType="email-address"
         />
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="phone-pad"
           keyboardType="phone-pad"
         />
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="ascii-capable"
           keyboardType="ascii-capable"
         />
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="numbers-and-punctuation"
           keyboardType="numbers-and-punctuation"
         />
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="url"
           keyboardType="url"
         />
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="number-pad"
           keyboardType="number-pad"
         />
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="name-phone-pad"
           keyboardType="name-phone-pad"
         />
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="decimal-pad"
           keyboardType="decimal-pad"
         />
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="twitter"
           keyboardType="twitter"
         />
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="web-search"
           keyboardType="web-search"
         />
         <TextInput
           style={styles.input}
-          onChangeText={onChange}
-          value={data}
+          onChangeText={setInputValue}
+          value={inputValue}
           placeholder="visible-password"
           keyboardType="visible-password"
         />
