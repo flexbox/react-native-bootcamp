db23eab5f1a57adabae3fe95b34e57d93674584f
David Leuliette <dleuliette@gmail.com>
2022-01-02 16:32:28 +0100

    add love with README

--- hackathon/allKeyboard/App.tsx
+++ hackathon/allKeyboard/App.tsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { TextInput } from "react-native-paper";
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import React from 'react';
+import { Card, Colors, Paragraph, TextInput, Title } from 'react-native-paper';
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 
-const UselessTextInput = () => {
+const CardInput = ({ title, children }) => {
+  return (
+    <Card style={styles.cardContainer}>
+      <Card.Content>
+        <Title>{title}</Title>
+        {children}
+      </Card.Content>
+    </Card>
+  );
+};
+
+const App = () => {
   const [inputValue, setInputValue] = React.useState(null);
-  const [password, setPassword] = React.useState("");
+  const [password, setPassword] = React.useState('');
   const [isVisble, setIsVisible] = React.useState(true);
 
   function toggleSecureIcon() {
@@ -12,127 +23,146 @@ const UselessTextInput = () => {
   }
 
   return (
-    <SafeAreaView style={styles.global}>
-      <ScrollView>
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="Input"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="numeric"
-          keyboardType="numeric"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          value={password}
-          secureTextEntry={isVisble}
-          onChangeText={(value) => setPassword(value)}
-          right={
-            <TextInput.Icon
-              style={{ marginTop: 25 }}
-              onPress={toggleSecureIcon}
-              name={isVisble ? "eye-off" : "eye"}
-            />
-          }
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="email-address"
-          keyboardType="email-address"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="phone-pad"
-          keyboardType="phone-pad"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="ascii-capable"
-          keyboardType="ascii-capable"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="numbers-and-punctuation"
-          keyboardType="numbers-and-punctuation"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="url"
-          keyboardType="url"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="number-pad"
-          keyboardType="number-pad"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="name-phone-pad"
-          keyboardType="name-phone-pad"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="decimal-pad"
-          keyboardType="decimal-pad"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="twitter"
-          keyboardType="twitter"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="web-search"
-          keyboardType="web-search"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setInputValue}
-          value={inputValue}
-          placeholder="visible-password"
-          keyboardType="visible-password"
-        />
+    <SafeAreaView style={styles.bgContainer}>
+      <ScrollView style={styles.container}>
+        <CardInput title="TextInput">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="A simple TextInput"
+          />
+        </CardInput>
+        <CardInput title="TextInput - `numeric`">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="numeric"
+            keyboardType="numeric"
+            autoComplete={false}
+          />
+        </CardInput>
+        <CardInput title="TextInput - `password`">
+          <TextInput
+            placeholder="********"
+            value={password}
+            secureTextEntry={isVisble}
+            onChangeText={(value) => setPassword(value)}
+            right={
+              <TextInput.Icon
+                style={{ marginTop: 8 }}
+                onPress={toggleSecureIcon}
+                name={isVisble ? 'eye-off' : 'eye'}
+              />
+            }
+          />
+        </CardInput>
+        <CardInput title="TextInput - `email-address`">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="email-address"
+            keyboardType="email-address"
+          />
+        </CardInput>
+        <CardInput title="TextInput - `phone-pad`">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="phone-pad"
+            keyboardType="phone-pad"
+          />
+        </CardInput>
+        <CardInput title="TextInput - `ascii-capable`">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="ascii-capable"
+            keyboardType="ascii-capable"
+          />
+        </CardInput>
+        <CardInput title="TextInput - `numbers-and-punctuation`">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="numbers-and-punctuation"
+            keyboardType="numbers-and-punctuation"
+          />
+        </CardInput>
+        <CardInput title="TextInput - `url`">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="url"
+            keyboardType="url"
+          />
+        </CardInput>
+        <CardInput title="TextInput - `number-pad`">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="number-pad"
+            keyboardType="number-pad"
+          />
+        </CardInput>
+        <CardInput title="TextInput - `name-phone-pad`">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="name-phone-pad"
+            keyboardType="name-phone-pad"
+          />
+        </CardInput>
+        <CardInput title="TextInput - `decimal-pad`">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="decimal-pad"
+            keyboardType="decimal-pad"
+          />
+        </CardInput>
+        <CardInput title="TextInput - `twitter`">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="@flexbox_"
+            keyboardType="twitter"
+          />
+        </CardInput>
+        <CardInput title="TextInput - `web-search`">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="web-search"
+            keyboardType="web-search"
+          />
+        </CardInput>
+        <CardInput title="TextInput - `visible-password`">
+          <TextInput
+            onChangeText={setInputValue}
+            value={inputValue}
+            placeholder="visible-password"
+            keyboardType="visible-password"
+          />
+        </CardInput>
+        <View style={styles.separator} />
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+  bgContainer: {
+    backgroundColor: Colors.grey100,
   },
-  global: {
-    paddingTop: 50,
+  cardContainer: {
+    marginBottom: 32,
+  },
+  container: {
+    paddingHorizontal: 24,
+  },
+  separator: {
+    height: 128,
   },
 });
 
-export default UselessTextInput;
+export default App;
---
+++ hackathon/allKeyboard/README.md
@@ -0,0 +1,14 @@
+# `allKeyboard`
+
+- All keyboard types on React Native
+
+## Result
+
+![React Native keyboard preview](./assets/example.gif)
+
+## Getting Started
+
+```console
+yarn
+yarn start
+```
