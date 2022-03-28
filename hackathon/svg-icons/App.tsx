3afb4270f87acf69ca63e00d1845f1f52f7395d9
David Leuliette <dleuliette@gmail.com>
2022-03-28 18:43:31 +0200

    fix icons path and config

--- hackathon/svg-icons/App.tsx
+++ hackathon/svg-icons/App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { VrProgram } from "./src/components/icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <VrProgram color={"red"} width={256} height={256} />
     </View>
   );
 }
@@ -13,8 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
