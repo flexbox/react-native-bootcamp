bb1cd10799dfdbae2529b4b0952badf024a11bcf
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-03-30 13:46:10 +0200

    add svg from figma

--- hackathon/svg-icons/App.tsx
+++ hackathon/svg-icons/App.tsx
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
-import { VrProgram } from "./src/components/icons";
+import { RoboticHand, VrProgram } from "./src/components/icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <VrProgram color={"red"} width={256} height={256} />
+      <VrProgram color={"red"} />
+      <RoboticHand color={"blue"} />
     </View>
   );
 }
