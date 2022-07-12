20a92dbb5547a040447caed7b3c259eab4759687
David Leuliette <dleuliette@gmail.com>
2022-07-12 13:49:43 +0200

    it works

--- hackathon/revolut-kids-clone/App.tsx
+++ hackathon/revolut-kids-clone/App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-red-500">
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
