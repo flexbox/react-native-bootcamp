1803067491f9d7baf3ecdcc3a04f55cb75931744
David Leuliette <dleuliette@gmail.com>
2022-08-09 14:48:03 +0200

    add button with PRessable

--- hackathon/revolut-kids-clone/src/components/Button.tsx
+++ hackathon/revolut-kids-clone/src/components/Button.tsx
@@ -1,4 +1,4 @@
-import type { PressableProps } from "react-native";
+import { PressableProps, View } from "react-native";
 import { Text, Pressable } from "react-native";
 import type { ReactNode } from "react";
 import React from "react";
@@ -9,29 +9,58 @@ interface Props extends PressableProps {
   children: ReactNode;
 }
 
-export const Button = ({ variant, children, styles = "", ...props }: Props) => {
-  let style = "bg-black hover:bg-indigo-700";
-  let textStyle = "text-white text-base font-medium";
+export const Button = ({
+  variant = "filled",
+  children,
+  styles = "",
+  ...props
+}: Props) => {
+  let style = "";
+  let textStyle = "text-base font-bold px-10 py-3";
+  let textColorVariant = "";
 
   switch (variant) {
-    case "primary":
+    case "dark":
+      style = "bg-black";
+      textColorVariant = "text-white";
       break;
-    case "secondary":
+    case "light":
+      style = "bg-white";
+      textColorVariant = "text-black";
+      break;
+    case "filled":
+      style = "bg-blue-500 hover:bg-blue-700";
+      textColorVariant = "text-white";
+      break;
+    case "outline":
+      style = "bg-blue-100 hover:bg-blue-700";
+      textColorVariant = "text-blue-500";
+      break;
+    case "light":
       style = "bg-white hover:bg-indigo-700";
       textStyle = "text-black text-base font-medium";
       break;
-
     default:
       break;
   }
 
   const box =
-    "inline-flex items-center px-10 py-3 border border-transparent rounded-xl shadow-sm";
+    "inline-flex items-center border border-transparent rounded-xl shadow-sm";
   const classNames = `${styles} ${box} ${style}`;
+  const textClassNames = `${textStyle} ${textColorVariant}`;
+
+  // TODO: add variant styles when pressed
+  // const activeClassNames = "bg-red-300";
 
   return (
     <Pressable className={classNames} {...props}>
-      <Text className={textStyle}>{children}</Text>
+      {/* {({ pressed }) => (
+        <View className={pressed && activeClassNames}>
+          <Text className={textClassNames}>{children}</Text>
+        </View>
+      )} */}
+
+      <Text className={textClassNames}>{children}</Text>
     </Pressable>
   );
 };
