2bf385f50ea4c74ae2d4540299364d180e1dd3a7
David Leuliette <dleuliette@gmail.com>
2022-08-19 13:33:48 +0200

    add style to sign in screen

--- hackathon/revolut-kids-clone/src/screens/LoginScreen.tsx
+++ hackathon/revolut-kids-clone/src/screens/LoginScreen.tsx
@@ -1,5 +1,7 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
 import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Button } from "../components/Button";
 import { ScreenContainer } from "../components/ScreenContainer";
@@ -8,11 +10,31 @@ import { Text } from "../components/Text";
 export const LoginScreen = ({ navigation }) => {
   return (
     <ScreenContainer withGoBack>
-      <View className="p-4 mt-8">
-        <Text variant="h1">SignIn</Text>
-        <Text variant="p1">To Sign In </Text>
-        <Text variant="p1">Where to find Code?</Text>
-      </View>
+      <SafeAreaView edges={["bottom", "top"]}>
+        <View className="m-auto justify-between h-screen">
+          <View className="mb-32">
+            <Text variant="h1" style="mb-4">
+              Sign in
+            </Text>
+            <Text variant="p1">
+              To sign in to the app you'll need to scan a code displayed on your
+              parents's phone.
+            </Text>
+            <Text variant="p1">Where to find Code?</Text>
+          </View>
+          <View className="mx-auto">
+            <MaterialCommunityIcons
+              name="qrcode-scan"
+              size={128}
+              color="#8463fc"
+            />
+          </View>
+          <View className="m-auto justify-bottom">
+            <Button styles="mb-4">Start camera</Button>
+            <Button variant="outline">Log in with mobile number</Button>
+          </View>
+        </View>
+      </SafeAreaView>
     </ScreenContainer>
   );
 };
