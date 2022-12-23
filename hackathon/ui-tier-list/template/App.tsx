aef9efc2c04dffae534dac549261f0e6000613fd
David Leuliette <dleuliette@gmail.com>
2022-12-23 12:05:24 +0100

    add react-native-safe-area-context

--- hackathon/ui-tier-list/template/App.tsx
+++ hackathon/ui-tier-list/template/App.tsx
@@ -1,21 +1,23 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+import { LoginScreen } from "./LoginScreen";
 
 // eslint-disable-next-line import/no-default-export
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaProvider>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <LoginScreen />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
