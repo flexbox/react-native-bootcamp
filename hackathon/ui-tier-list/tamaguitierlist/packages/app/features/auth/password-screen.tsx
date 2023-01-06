9e6599f60fd08f0e8182654d8f449542648e7891
David Leuliette <dleuliette@gmail.com>
2023-01-06 14:10:55 +0100

    add login screen

---
+++ hackathon/ui-tier-list/tamaguitierlist/packages/app/features/auth/password-screen.tsx
@@ -0,0 +1,14 @@
+import { Button, H1, H3, Paragraph, YStack } from '@my/ui'
+import React from 'react'
+
+export function PasswordScreen() {
+  return (
+    <YStack f={1} jc="center" ai="center" space>
+      <Paragraph ta="center" fow="800">
+        Reset Password
+      </Paragraph>
+      <H1>Hello</H1>
+      <H3>Hello</H3>
+    </YStack>
+  )
+}
--- hackathon/ui-tier-list/tamaguitierlist/packages/app/features/home/screen.tsx
+++ hackathon/ui-tier-list/tamaguitierlist/packages/app/features/home/screen.tsx
@@ -1,28 +1,58 @@
-import { Anchor, Button, H1, Input, Paragraph, Separator, Sheet, XStack, YStack } from '@my/ui'
+import { Button, H1, Input, Paragraph, ScrollView, useTheme, XStack, YStack } from '@my/ui'
 import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
 import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { useLink } from 'solito/link'
 
 export function HomeScreen() {
-  const linkProps = useLink({
+  const linkUserProps = useLink({
     href: '/user/nate',
   })
+  const linkPassordProps = useLink({
+    href: '/password',
+  })
+
+  const theme = useTheme()
 
   return (
-    <YStack f={1} jc="center" ai="center" p="$4" space>
-      <YStack space="$4" maw={600}>
-        <H1 ta="center">Welcome</H1>
-        <Paragraph ta="center">
-          Here's a basic starter to show navigating from one screen to another. This screen uses the
-          same code on Next.js and React Native.
-        </Paragraph>
-        <Input size="$4" borderWidth={2} />
-        <Input placeholder="Password" secureTextEntry={true} borderWidth={2} returnKeyType="done" />
+    <ScrollView>
+      <YStack space="$6" padding="$4" backgroundColor={'#3B82F6'}>
+        <SafeAreaView>
+          <H1 ta="center" color={'#ffffff'}>
+            Hello
+          </H1>
+          <Paragraph ta="center">Sign in to your account</Paragraph>
+        </SafeAreaView>
       </YStack>
+      <YStack f={1} p="$4" space>
+        <YStack space="$4">
+          <Input
+            placeholder="Email"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            autoCapitalize="none"
+            autoCorrect={false}
+            autoComplete="email"
+            size="$4"
+            borderWidth={2}
+          />
+          <Input
+            placeholder="Password"
+            secureTextEntry={true}
+            borderWidth={2}
+            returnKeyType="done"
+          />
+        </YStack>
 
-      <XStack>
-        <Button {...linkProps}>Link to user</Button>
-      </XStack>
-    </YStack>
+        <YStack>
+          <Button {...linkUserProps} size="$4" theme="blue_Button" f={1}>
+            Sign In
+          </Button>
+          <Button {...linkPassordProps} size="$4" chromeless mt="$4" f={1}>
+            Don't have an account? Create
+          </Button>
+        </YStack>
+      </YStack>
+    </ScrollView>
   )
 }
