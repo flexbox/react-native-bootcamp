9e809661612995d78347886277259c798b4ae26d
David Leuliette <dleuliette@gmail.com>
2023-01-06 14:44:57 +0100

    add theme

--- hackathon/ui-tier-list/Dripsy/App.tsx
+++ hackathon/ui-tier-list/Dripsy/App.tsx
@@ -1,18 +1,22 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import { DripsyProvider } from "dripsy";
 
 import { LoginScreen } from "./LoginScreen";
+import { theme } from "./theme";
 
 // eslint-disable-next-line import/no-default-export
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar style="auto" />
-      <View style={styles.container}>
-        <LoginScreen />
-      </View>
-    </SafeAreaProvider>
+    <DripsyProvider theme={theme}>
+      <SafeAreaProvider>
+        <StatusBar style="auto" />
+        <View style={styles.container}>
+          <LoginScreen />
+        </View>
+      </SafeAreaProvider>
+    </DripsyProvider>
   );
 }
 
--- hackathon/ui-tier-list/Dripsy/LoginScreen.tsx
+++ hackathon/ui-tier-list/Dripsy/LoginScreen.tsx
@@ -1,12 +1,35 @@
-import { View, Text } from "react-native";
+import { View, Text, H2, P } from "dripsy";
 import React from "react";
 
 interface Props {}
 
 export const LoginScreen = (props: Props) => {
   return (
-    <View style={{ alignItems: "center", flex: 1, justifyContent: "center" }}>
-      <Text>LoginScreen</Text>
+    <View sx={{ flex: 1 }}>
+      <View
+        sx={{
+          backgroundColor: "$primary",
+          padding: ["$5", "$8"],
+        }}
+      >
+        <H2>LoginScreen</H2>
+        <P>LoginScreen</P>
+      </View>
+      <View
+        sx={{
+          padding: ["$1", "$3"],
+        }}
+      >
+        {/* <Button label="Sign In" onPress={() => Alert.alert("Pressed")} />
+        <Button
+          mt="xl"
+          borderRadius="m"
+          backgroundColor="cardPrimaryBackground"
+          padding="m"
+          label="Don't have an account? Create"
+          onPress={() => Alert.alert("Pressed")}
+        /> */}
+      </View>
     </View>
   );
 };
