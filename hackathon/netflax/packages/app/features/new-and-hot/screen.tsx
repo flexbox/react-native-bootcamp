181e39aa44fa6878f5b7ca5f890b074ba23ef630
David Leuliette <dleuliette@gmail.com>
2023-03-17 15:50:32 +0100

    add tabs

--- hackathon/netflax/packages/app/features/new-and-hot/screen.tsx
+++ hackathon/netflax/packages/app/features/new-and-hot/screen.tsx
@@ -1,25 +1,15 @@
-import { YStack, Text, H3, H4, Paragraph, H2 } from '@my/ui'
+import { YStack, H3, H4, Paragraph, H2 } from '@my/ui'
 import React from 'react'
-import { format, formatDistance, formatRelative, subDays } from 'date-fns'
 
-interface Props {}
+import { DiscoverTabs } from 'app/features/new-and-hot/discover-tabs'
+import { UpcomingDate } from 'app/features/new-and-hot/upcoming-date'
 
-export const NewAndHotScreen = (props: Props) => {
-  const currentMonth = format(new Date(), 'M')
-  const currentYear = format(new Date(), 'yyyy')
-  const nextMonth = format(new Date(Number(currentYear), Number(currentMonth), 1), 'MMM')
-
+export const NewAndHotScreen = () => {
   return (
     <YStack>
       <H3>New & Hot</H3>
-      <Text>Tabs</Text>
-      <H4>🍿 Coming Soon</H4>
-      <Paragraph textTransform="uppercase" fontWeight="800">
-        {nextMonth}
-      </Paragraph>
-      <H3 textTransform="uppercase" fontWeight="800">
-        29
-      </H3>
+      <DiscoverTabs />
+      <UpcomingDate />
     </YStack>
   )
 }
