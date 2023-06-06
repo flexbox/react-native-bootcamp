06f044cee900bfa3169dceb9dd17d3cee53ea6e4
David Leuliette <dleuliette@gmail.com>
2023-06-06 13:09:05 +0200

    add TypeScript `const` assertions for react-navigation

--- hackathon/spacecraft/src/navigation/Routes.ts
+++ hackathon/spacecraft/src/navigation/Routes.ts
@@ -1,15 +1,18 @@
-export enum Routes {
-  LOGIN_SCREEN = "Login",
-  TERMS_SCREEN = "Terms",
-  STARSHIP_FEED_SCREEN = "Starships",
-  STARSHIP_STACK = "Starships Stack",
-  STARSHIP_DETAILS_SCREEN = "Starship",
-  PILOT_SCREEN = "Pilots",
-  PILOT_STACK = "Pilots Stack",
-  PILOT_DETAILS_SCREEN = "Pilot",
-  PLUS_STACK = "Plus Stack",
-  PLUS_SCREEN = "Plus",
-  DO_YOU_LIKE_SCREEN = "Do You Like",
-  LOVE_SCREEN = "Love",
-  DISAPPOINTED_SCREEN = "Disappointed",
-}
+export const Routes = {
+  LOGIN_SCREEN: "Login",
+  TERMS_SCREEN: "Terms",
+  STARSHIP_FEED_SCREEN: "Starships",
+  STARSHIP_STACK: "Starships Stack",
+  STARSHIP_DETAILS_SCREEN: "Starship",
+  PILOT_SCREEN: "Pilots",
+  PILOT_STACK: "Pilots Stack",
+  PILOT_DETAILS_SCREEN: "Pilot",
+  PLUS_STACK: "Plus Stack",
+  PLUS_SCREEN: "Plus",
+  DO_YOU_LIKE_SCREEN: "Do You Like",
+  LOVE_SCREEN: "Love",
+  DISAPPOINTED_SCREEN: "Disappointed",
+} as const;
+
+type ObjectValues<T extends object> = T[keyof T];
+export type RouteNames = ObjectValues<typeof Routes>;
