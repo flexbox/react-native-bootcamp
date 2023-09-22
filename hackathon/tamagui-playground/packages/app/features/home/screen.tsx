7e11cde6af8e14ba6fb43056d7b9594315c4cd71
David Leuliette <dleuliette@gmail.com>
2023-09-22 21:25:02 +0200

    🔮 udpate

--- hackathon/tamagui-playground/packages/app/features/home/screen.tsx
+++ hackathon/tamagui-playground/packages/app/features/home/screen.tsx
@@ -5,35 +5,35 @@ import { Link } from 'solito/link'
 
 export function HomeScreen() {
   return (
-    <YStack f={1} jc="center" ai="center" p="$4" space>
-      <YStack space="$4" maw={600}>
+    <YStack ai="center" p="$4" space>
+      <YStack space maw={600}>
         <H1 ta="center">Tamagui Playground</H1>
 
         <Link href="/basic/">
-          <H2>BASIC</H2>
+          <H2>Basic</H2>
           <Paragraph> Stacks Headings Text</Paragraph>
         </Link>
 
-        <H2>ORGANIZE</H2>
+        <H2>Forms</H2>
         <Paragraph>
           Button Checkbox Form Input & TextArea Label Progress RadioGroup Select Slider Switch
           ToggleGroup
         </Paragraph>
 
-        <H2>CONTENT</H2>
+        <H2>Panels</H2>
         <Paragraph>AlertDialog Dialog Popover Sheet Tooltip Toast</Paragraph>
 
-        <H2></H2>
+        <H2>Organize</H2>
         <Paragraph>Accordion Group Tabs</Paragraph>
-        <H2></H2>
+
+        <H2>Content</H2>
         <Paragraph>Avatar Card Image Listltem</Paragraph>
-        <H2></H2>
+
+        <H2>Visual</H2>
         <Paragraph>LinearGradient Separator Square & Circle</Paragraph>
 
-        <H2>ETC</H2>
+        <H2>Etc</H2>
         <Paragraph> Anchor HTML Elements ScrollView Spinner Unspaced VisuallyHidden</Paragraph>
-
-        <Separator />
       </YStack>
     </YStack>
   )
