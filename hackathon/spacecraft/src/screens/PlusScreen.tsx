070404aa4fce6e7c0407e574eaf12f5ecbdecfa0
David Leuliette <dleuliette@gmail.com>
2023-12-14 16:30:13 +0100

    add version app number

--- hackathon/spacecraft/src/screens/PlusScreen.tsx
+++ hackathon/spacecraft/src/screens/PlusScreen.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { List } from "react-native-paper";
+import { List, Text } from "react-native-paper";
+import * as Application from "expo-application";
+import { View } from "react-native";
 
 import { ScreenContainer } from "~/components/ScreenContainer";
 import { Routes } from "~/navigation/Routes";
@@ -13,11 +15,25 @@ export const PlusScreen = ({ navigation }: Props) => {
 
   return (
     <ScreenContainer title={"Plus"}>
-      <List.Item
-        title="Do you like Spacecraft?"
-        onPress={navigateToDoYouLikeScreen}
-        left={(props) => <List.Icon {...props} icon="star" />}
-      />
+      <View style={{ flex: 1 }}>
+        <List.Item
+          title="Do you like Spacecraft?"
+          onPress={navigateToDoYouLikeScreen}
+          left={(props) => <List.Icon {...props} icon="star" />}
+        />
+      </View>
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          marginBottom: 24,
+        }}
+      >
+        <Text variant="bodySmall">Built in France by David Leuliette</Text>
+        <Text variant="bodySmall">
+          {Application.applicationName} {Application.nativeBuildVersion}
+        </Text>
+      </View>
     </ScreenContainer>
   );
 };
