fb0faacdd8c503f36ad016a51def5324eb7e2070
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-03-06 12:36:01 +0100

    fix: 🐛 migrate from storybook-6.5 to 7
    ✅ Closes: #222

--- hackathon/spacecraft/.storybook/main.js
+++ hackathon/spacecraft/.storybook/main.js
@@ -3,5 +3,10 @@ module.exports = {
     "../src/components/**/*.stories.?(ts|tsx|js|jsx)",
     "../src/screens/**/*.stories.?(ts|tsx|js|jsx)",
   ],
-  addons: ["@storybook/addon-ondevice-controls"],
+  addons: [
+    '@storybook/addon-ondevice-notes',
+    '@storybook/addon-ondevice-controls',
+    '@storybook/addon-ondevice-backgrounds',
+    '@storybook/addon-ondevice-actions',
+  ],
 };
