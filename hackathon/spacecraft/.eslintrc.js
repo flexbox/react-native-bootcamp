64ba39a6fcdc05f4a923f3d60b5e3df7289eb955
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-05-27 15:59:59 +0200

    fix: 🐛 update eslint config
    ✅ Closes: #230

--- hackathon/spacecraft/.eslintrc.js
+++ hackathon/spacecraft/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["expo", "react-native-wcandillon"],
   plugins: ["@tanstack/query"],
+  requireConfigFile: false,
   rules: {
     camelcase: "off",
     "import/no-anonymous-default-export": [1, { allowArray: true }],
