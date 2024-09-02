dc4ab4ae43e0a147468ae73295ec3fb52e2144f5
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-09-02 17:01:11 +0200

    feat: 🎸 add perfectionnist to eslint config
    ✅ Closes: #251

--- hackathon/spacecraft/.eslintrc.js
+++ hackathon/spacecraft/.eslintrc.js
@@ -2,49 +2,57 @@ module.exports = {
   env: {
     node: true,
   },
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  root: true, // make sure eslint picks up the config at the root of the directory
   extends: [
     "expo",
     "eslint:recommended", // ESLint rules
     "plugin:@typescript-eslint/recommended", // TypeScript rules
     "plugin:react/recommended", // React rules
-    "plugin:react/jsx-runtime", // support for React 17 JSX
+    "plugin:react/jsx-runtime", // Support for React 17 JSX
     "plugin:prettier/recommended", // Prettier recommended rules
   ],
-  plugins: ["react", "react-native", "simple-import-sort"], // add React and React Native plugins
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  plugins: [
+    "react",
+    "react-native",
+    "simple-import-sort",
+    "perfectionist", // Existing plugins plus 'perfectionist'
+  ],
+  root: true, // Make sure eslint picks up the config at the root of the directory
   rules: {
-    camelcase: "off", // disable camelcase rule
-    "@typescript-eslint/no-explicit-any": "warn", // detect usage of `any` type
+    "@typescript-eslint/no-explicit-any": "warn", // Detect usage of `any` type
+    "@typescript-eslint/no-unused-vars": "warn", // Detect unused variables
+    camelcase: "off", // Disable camelcase rule
+    "perfectionist/sort-imports": "error", // Previous perfectionist rule
+    "perfectionist/sort-interfaces": ["error"], // New perfectionist rule
+    "perfectionist/sort-objects": ["error", { type: "alphabetical" }], // New perfectionist rule
     "prettier/prettier": [
-      // Prettier rules
       "warn",
       {
         usePrettierrc: true,
       },
     ],
-    "react-native/no-color-literals": 2, // enforce color literals are not used
-    "react-native/no-unused-styles": 2, // detect unused StyleSheet rules
-    "react-native/no-raw-text": 0, // detect raw text outside of Text component
-    "react-native/sort-styles": 2, // enforce style definitions are sorted
-    "@typescript-eslint/no-unused-vars": "warn", // detect unused variables
-    "simple-import-sort/exports": "warn", // enforce sorting exports within module
+    "react-native/no-color-literals": 2, // Enforce color literals are not used
+    "react-native/no-raw-text": 0, // Detect raw text outside of Text component
+    "react-native/no-unused-styles": 2, // Detect unused StyleSheet rules
+    "react-native/sort-styles": 2, // Enforce style definitions are sorted
+    "simple-import-sort/exports": "warn", // Enforce sorting exports within module
     "simple-import-sort/imports": [
       "warn",
       {
         groups: [
-          // Side effect imports.
           ["^\\u0000"],
-          // Packages `react` related packages come first.
           ["^react", "^@?\\w"],
-          // Environment variables
           ["^(@env)(/.*|$)"],
-          // Parent imports. Put `..` last.
           ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
-          // Other relative imports. Put same-folder imports and `.` last.
           ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
         ],
       },
     ],
+  },
+  settings: {
+    perfectionist: {
+      partitionByComment: true,
+      type: "line-length",
+    },
   },
 };
