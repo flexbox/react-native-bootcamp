04d89c7b0f086e7f6d6eb4b8a347b3e64c624427
David Leuliette <dleuliette@gmail.com>
2024-09-18 15:36:05 +0200

    add docs

--- hackathon/spacecraft/src/context/Authentication.tsx
+++ hackathon/spacecraft/src/context/Authentication.tsx
@@ -11,9 +11,24 @@ const AuthenticationContext = createContext<AuthenticationContextProps>(
   {} as AuthenticationContextProps,
 );
 
+/**
+ * AuthenticationProvider component that provides authentication context to its children.
+ *
+ * @param {PropsWithChildren} props - The props for the component, including children.
+ *
+ * @remarks
+ * This component uses a boolean state to represent the user's authentication status.
+ * The `setUser` function is provided to update the authentication status.
+ *
+ * @example
+ * ```tsx
+ * <AuthenticationProvider>
+ *   <YourComponent />
+ * </AuthenticationProvider>
+ * ```
+ */
 export const AuthenticationProvider = ({ children }: PropsWithChildren) => {
   const [user, setUser] = useState<boolean>(false);
-  // console.log("file: Authentication.tsx ~ line 19 ~ user", user);
 
   return (
     <AuthenticationContext.Provider value={{ setUser, user }}>
